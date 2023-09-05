e987d3d424736a61a9b84260816b1a5121cf90b2
muhammadjewel <msuratov@gmail.com>
2023-09-05 21:10:32 +0500

    Refactor email validation

--- js/main.js
+++ js/main.js
@@ -1,35 +1,40 @@
 const MODIFIERS = {
   formGroupHasError: 'form__group--has-error'
-}
+};
+const REGEX_EMAIL = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 function init () {
   const elAccessRequestForm = document.querySelector('#access-request-form');
   const elEmailField = document.querySelector('.form__field');
+  const elFormErrorMessage = elAccessRequestForm.querySelector('.form__error-message');
+
+
+  function removeErrorMessage() {
+    elEmailField.parentNode.classList.remove(MODIFIERS.formGroupHasError);
+  }
+
+  function displayErrorMessage(errorMessage) {
+    elEmailField.parentNode.classList.add(MODIFIERS.formGroupHasError);
+    elFormErrorMessage.textContent = errorMessage;
+    return errorMessage;
+  }
+
 
   if (elAccessRequestForm) {
     elAccessRequestForm.addEventListener('submit', function (evt) {
       evt.preventDefault();
-      elEmailField.parentNode.classList.remove(MODIFIERS.formGroupHasError);
-
-      const elFormErrorMessage = elAccessRequestForm.querySelector('.form__error-message');
-      let errorMessage;
+      removeErrorMessage();
 
       // Check for empty field of .form__field
       if (!elEmailField.value) {
-        elEmailField.parentNode.classList.add(MODIFIERS.formGroupHasError);
-        errorMessage = 'Oops! Please add your email';
-        elFormErrorMessage.textContent = errorMessage
+        displayErrorMessage('Oops! Please add your email');
         return;
       }
 
       // Check for valid email address
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      const isEmailValid = emailRegex.test(elEmailField.value);
-
+      const isEmailValid = REGEX_EMAIL.test(elEmailField.value);
       if (!isEmailValid) {
-        elEmailField.parentNode.classList.add(MODIFIERS.formGroupHasError);
-        errorMessage = 'Oops! Please check your email';
-        elFormErrorMessage.textContent = errorMessage
+        displayErrorMessage('Oops! Please check your email');
         return;
       }
     });
